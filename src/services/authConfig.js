fc56ab9815a1c870a8b066d214fe9c8da3a7b040
eduard165 <112569276+eduard165@users.noreply.github.com>
2024-09-30 20:56:17 -0600

    Error de redireccion de autenticacion

--- src/services/authConfig.js
+++ src/services/authConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
   auth: {
     clientId: process.env.NEXT_PUBLIC_AZURE_AD_CLIENT_ID,
     authority: process.env.NEXT_PUBLIC_AZURE_AD_AUTHORITY,  // Configura la autoridad desde el entorno
-    redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI || 'http://localhost:3000',  // Local en desarrollo, variable en producción
+    redirectUri: process.env.NEXT_PUBLIC_REDIRECT_URI,  // Local en desarrollo, variable en producción
   },
   cache: {
     cacheLocation: "localStorage",  // Mantener el estado de la sesión en localStorage
